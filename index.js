320e9cb9dbc49d2fc928d1a225248baf62f721fe
PrantaAcerjee <prantaacharjee647@gmail.com>
2021-10-30 07:31:45 -0700

    delete added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId=require('mongodb').ObjectId;
 const app=express();
 const cors=require('cors');
 const port=process.env.PORT||5000;
@@ -10,7 +11,51 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrj86.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+async function run(){
+try{
+await client.connect();
+console.log('database connected sucessfully')
+const database=client.db('travelsFreak');
+const servicesCollection=database.collection('services');
+// Get Api 
+app.get('/services',async (req,res)=>{
+    const cursor=servicesCollection.find({});
+    const services=await cursor.toArray();
+    res.send(services);
+})
+
+//Get single service
+app.get('/services/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id:ObjectId(id)};
+
+    const service=await servicesCollection.findOne(query)
+    res.json(service);
+})
+
+// post api 
+app.post('/services',async(req,res)=>{
+
+const service=req.body;
+//  console.log('hit the post',service)
+const result=await servicesCollection.insertOne(service)
+res.json(result)
+})
+//delete api
+app.delete('/services/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id:ObjectId(id)};
+    const result=await servicesCollection.deleteOne(query);
+    res.json(result);
+})
+
+}
+finally{
+    // await client.close();
+}
+
+};
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('server run on')
